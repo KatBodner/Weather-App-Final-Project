882450c4ba354e685a0050eb22b0d77b58831e32
KatBodner <katrin.bodner@hotmail.de>
2022-06-12 13:23:49 +0200

    insert basic forecast js

--- src/index.js
+++ src/index.js
@@ -39,6 +39,38 @@ function showCity(searchInput) {
   axios.get(apiUrl).then(showWeather);
 }
 
+displayForecast();
+
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"]
+  days.forEach(function (day) {
+    
+      forecastHTML = forecastHTML +
+        `
+        <div class="col-2">
+          <div class="weather-forecast-date">${day}</div>
+          <img
+            src="http://openweathermap.org/img/wn/50d@2x.png"
+            alt=""
+            width="42"
+          />
+        </br>
+          <div class="weather-forecast-temperatures">
+            <span class="weather-forecast-temperature-max"> 18° </span>
+            <span class="weather-forecast-temperature-min"> 12° </span>
+          </div>
+        </div>
+      `;    
+    }
+  );
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function showWeather(response) {
     document.querySelector("#city").innerHTML = response.data.name;
     let temperature = Math.round(response.data.main.temp);
