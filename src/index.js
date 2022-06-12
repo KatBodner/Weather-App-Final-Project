e748e642ab28bf0d35f1998ab9ff92b750ef1098
KatBodner <katrin.bodner@hotmail.de>
2022-06-12 14:04:36 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -75,6 +75,7 @@ function getForecast(coordinates) {
     let apiKey = "5d2f35015df0ea7c61fd5ee9a6f153b5";
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayForecast);
+}
 
 function showWeather(response) {
     document.querySelector("#city").innerHTML = response.data.name;
@@ -95,4 +96,4 @@ function showWeather(response) {
       iconElement.setAttribute("alt", response.data.weather[0].description);
 
       getForecast(response.data.coord);
-  }
+    }
