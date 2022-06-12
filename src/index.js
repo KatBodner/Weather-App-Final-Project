c135e9dbb25b18ffa3072cf794b493df78ee3403
KatBodner <katrin.bodner@hotmail.de>
2022-06-12 14:02:43 +0200

    integrate forecast API key and basic function

--- src/index.js
+++ src/index.js
@@ -71,6 +71,11 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+    let apiKey = "5d2f35015df0ea7c61fd5ee9a6f153b5";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
+
 function showWeather(response) {
     document.querySelector("#city").innerHTML = response.data.name;
     let temperature = Math.round(response.data.main.temp);
@@ -88,4 +93,6 @@ function showWeather(response) {
       `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
       );
       iconElement.setAttribute("alt", response.data.weather[0].description);
+
+      getForecast(response.data.coord);
   }
